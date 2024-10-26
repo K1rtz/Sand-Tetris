4d02d3beb3c5528d886e3bf074d6003cd6df9d56
K1rtz <djidicdavid@proton.me>
2024-10-26 18:27:29 +0200

    added circular spawning of dropping objects and ability to move them left and right during free fall

--- Tetris.js
+++ Tetris.js
@@ -16,8 +16,26 @@ export class Tetris{
 
         this.ctx
 
-        this.frameDelay = 200;
+        this.frameDelay = 20;
 
+        this.fell = false;
+        this.moveRight = false;
+        this.moveLeft = false;
+
+
+        this.registerKeyListeners()
+    }
+
+    registerKeyListeners(){
+        window.addEventListener('keydown', (event)=>{
+            if(event.key === 'ArrowLeft'){
+                this.moveLeft = true;
+                // console.log('levo')
+            } else if(event.key === 'ArrowRight'){
+                this.moveRight = true;
+                // console.log('desno')
+            }
+        })
     }
 
     start(){
@@ -31,61 +49,196 @@ export class Tetris{
         //this.cells.forEach(e=>e.show())
         for(let i = 0; i < this.cols; i++){
             for(let j = 0; j < this.rows; j++){
-                if(this.grid[i][j] == 0)
+                if(this.grid[i][j].block == false)
                     this.ctx.fillStyle = 'black'
+                
+                else if (this.grid[i][j].initial == true)
+                    this.ctx.fillStyle = 'green'
                 else
-                    this.ctx.fillStyle = 'white'
+                this.ctx.fillStyle = 'white'
+
                 this.ctx.fillRect(i*this.w,j*this.w,this.w, this.w);
-                this.grid2[i][j] = 0;
+                this.grid2[i][j] = {
+                                        x: i,
+                    y: j,
+                    color: 'background',
+                    block: false,
+                    initial: false
+                }
             }
         }
 
         for(let j = this.rows-1; j >= 0; j--){
             for(let i = this.cols-1; i >= 0; i--){  
-                //this.grid2[this.cols-4][this.rows-1] = 1;
 
                 let state = this.grid[i][j]
-                if(state === 1){
-                    console.log('x')
+                if(state.block == true){
                     let below = this.grid[i][j+1]
-                    if(below != undefined && below === 0){
+                    
+                    //AKO NEMA NISTA ISPOD BLOKA ON IDE JEDNO DOLE
+                    if(below != undefined && below.block == false){
+                        // console.log('x')
 
-                        this.grid[i][j] = 0;
-                        this.grid[i][j+1] = 1;
-                        this.grid2[i][j+1] = 1;
-                    }else if(below != undefined && below != 0){
-                        let dRight = this.grid[i+1][j+1]
-                        let dLeft = this.grid[i-1][j+1]
-                        if(dRight != undefined && dRight === 0){
-                            this.grid[i][j]  = 0
-                            this.grid[i+1][j+1]  = 1
-                            this.grid2[i+1][j+1]  = 1
+                        this.grid[i][j].block = false;
+                        // this.grid[i][j].initial = false;
+
+                        this.grid[i][j+1].block = true;
+                        // this.grid[i][j+1].initial = true;
+
+                        this.grid2[i][j+1].block = true;
+                        this.grid2[i][j+1].initial = this.grid[i][j].initial
+                    }
+                    //AKO IMA NESTO ISPOD BLOKA PROVERAVA DOLE LEVO I DOLE DESNO
+                    else if(below != undefined && below.block == true){
+                        let dRight;
+                        let dLeft;
+                        if(this.grid[i][j].initial){
+                            this.fell = true;
                         }
-                        else if(dLeft != undefined && dLeft === 0){
-                            this.grid[i][j]  = 0
-                            this.grid[i-1][j+1]  = 1
-                            this.grid2[i-1][j+1]  = 1
+
+                        if(i<59){
+                            dRight = this.grid[i+1][j+1]
+                        }else{
+                            dRight=undefined;
+                        }
+                        if(i>0){
+                            dLeft = this.grid[i-1][j+1]
+                        }else{
+                            dLeft = undefined;
+                        }
+                        //PROVERA DOLE DESNO
+                        if(dRight != undefined && dRight.block === false){
+                            this.grid[i][j].block  = false
+                            // this.grid[i][j].initial = false;
+
+                            this.grid[i+1][j+1].block  = true
+                            this.grid[i+1][j+1].initial = true;
+
+                            this.grid2[i+1][j+1].block  = true
+                            this.grid2[i+1][j+1].initial  = this.grid[i][j].initial
+
+
+                        }
+                        //PROVERA DOLE LEVO
+                        else if(dLeft != undefined && dLeft.block === false){
+                            this.grid[i][j].block  = false
+                            // this.grid[i][j].initial  = false
+
+                            this.grid[i-1][j+1].block  = true
+                            this.grid[i-1][j+1].initial  = true
+
+                            this.grid2[i-1][j+1].block  = true
+                            this.grid2[i-1][j+1].initial  = this.grid[i][j].initial
+
                         }
                         else{
-                            this.grid2[i][j] = 1;
+                            this.grid2[i][j].block = true;
+                            this.grid2[i][j].initial = this.grid[i][j].initial
+                            if(this.grid2[i][j].initial){
+                                this.fell = true;
+                            }
+
                         }
                     }
                     else{
-                        this.grid2[i][j] = 1;
+                        this.grid2[i][j].block = true;
+                        this.grid2[i][j].initial = this.grid[i][j].initial;
+                        
+                        if(this.grid2[i][j].initial){
+                            this.fell = true;
+                        }
                     }
+
                 }
+
+                    if(i==0 && j==0 && this.fell == true){
+                        this.grid2.forEach(e=>{
+                            e.forEach(e2 =>{
+                                if(e2.initial)
+                                e2.initial = false;
+                            })
+                        })
+                        this.grid.forEach(e=>{
+                            e.forEach(e2 =>{
+                                if(e2.initial)
+                                e2.initial = false;
+                            })
+                        }) 
+                    }
             }
         }
+
+
+        if(this.fell == true){
+            this.dodajFiguru()
+        }
+
+        this.checkForMoves()
+
         //console.log(this.grid[this.cols-1][this.rows])
         this.swapGrids()
 
         
     }
+    checkForMoves(){
+        if(this.moveRight){
+
+            for(let i = this.cols-2; i >= 0; i--){ 
+                for(let j = 0; j < this.rows; j++){
+
+                    if(this.grid2[i][j].block == true && this.grid2[i][j].initial == true &&  this.grid2[i+1][j].block == false){
+                        this.grid2[i][j].block = false
+                        this.grid2[i][j].initial = false
+
+                        this.grid2[i+1][j].block = true
+                        this.grid2[i+1][j].initial = true
+                    }
+                }
+            }
+
+
+
+            this.moveRight = false
+        }
+        if(this.moveLeft){
+            
+            for(let i = 1; i < this.cols; i++){  
+                for(let j = 0; j < this.rows; j++){
+                    if(this.grid2[i][j].block == true && this.grid2[i][j].initial == true && this.grid2[i-1][j].initial == false){
+                        this.grid2[i][j].block = false
+                        this.grid2[i][j].initial = false
+
+                        this.grid2[i-1][j].block = true
+                        this.grid2[i-1][j].initial = true
+                    }
+                }
+                console.log(this.cols)
+            }
+
+
+
+            this.moveLeft = false
+        }
+
+    }
+
+    dodajFiguru(){
+        this.fell = false;
+        //grid 2 vise nema initial true kockice
+
+        for(let i = 20; i < 30; i++){
+            for(let j = 10; j < 20; j++){
+                this.grid2[i][j].color = 'white';
+                this.grid2[i][j].block = true;
+                this.grid2[i][j].initial = true;
+            }
+        }
+
+        console.log('pozvano je dodaj figuru')
+    }
 
     swapGrids(){
         this.grid = this.grid2.map(row => row.slice());
-
-
     }
 
     createCanvas(){
@@ -95,57 +248,45 @@ export class Tetris{
         canvas.height = this.height;
         this.cols = this.width/this.w;
         this.rows = this.height/this.w;
-        this.grid = new Array(this.cols).fill(0).map(() => new Array(this.rows).fill(0));
-        this.grid2 = new Array(this.cols).fill(0).map(() => new Array(this.rows).fill(0));
+        // this.grid = new Array(this.cols).fill(0).map(() => new Array(this.rows).fill(0));
+        // this.grid2 = new Array(this.cols).fill(0).map(() => new Array(this.rows).fill(0));
 
+        this.grid = []
+        this.grid2 = []
         for(let i = 0; i< this.cols; i++){
+            this.grid[i] = []
+            this.grid2[i] = []
             for(let j = 0; j <this.rows; j++){
-                this.grid[i][j] = 0;
-                this.grid2[i][j] = 0;
+                this.grid[i][j] = {
+                    x: i,
+                    y: j,
+                    color: 'background',
+                    block: false,
+                    initial: false
+                }
+                this.grid2[i][j] = {
+                    x: i,
+                    y: j,
+                    color: 'background',
+                    block: false,
+                    initial: false
+                };
             }
         }
 
-        // this.grid[0][1]= 1;
-        // this.grid[0][2]= 1;
-        // this.grid[0][3]= 1;
-        // this.grid[0][4]= 1;
-        // this.grid[0][5]= 1;
 
-        // this.grid[4][90] = 1;
-        // this.grid[4][91] = 1;
-        // // this.grid[4][92] = 1;
-        // this.grid[5][90] = 1;
-        // this.grid[5][91] = 1;
-        // this.grid[5][92] = 1;
-        // this.grid[6][90] = 1;
-        // this.grid[6][91] = 1;
-        // this.grid[6][92] = 1;
-        // this.grid[10][17] = 1;
-        // this.grid[11][18] = 1;
-        // this.grid[11][17] = 1;
-        // this.grid[10][19] = 1;
-        // this.grid[11][19] = 1;
-        // this.grid[10][20] = 1;
-        // this.grid[11][20] = 1;
-        // this.grid[10][21] = 1;
-        // this.grid[10][22] = 1;
-        // this.grid[11][22] = 1;
-        // this.grid[12][22] = 1;
-        // this.grid[13][22] = 1;
-        // this.grid[11][21] = 1;
-        // this.grid[12][21] = 1;
-        // this.grid[14][22] = 1;
-        // this.grid[13][21] = 1;
-        // this.grid[14][21] = 1;
-        for(let i = 10; i<13; i++){
-            for(let j = 80; j<83;j++){
-                this.grid[i][j] = 1;
+        for(let i = 10; i<26; i++){
+            for(let j = 70; j<87;j++){
+                this.grid[i][j].color = 'white';
+                this.grid[i][j].block = true;
+                this.grid[i][j].initial = true;
             }
         }
 
         this.host.appendChild(canvas);
         this.ctx = canvas.getContext('2d');
     }
+
     animate(){
         //const frameDelay = 200;
         const currentTime = performance.now()
